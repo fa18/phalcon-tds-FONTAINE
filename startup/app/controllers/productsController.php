b070aaac56feb38fa68da58eba485c2bafaa11d7
Fabien Fontaine <Fabien Fontaine>
2017-03-15 16:02:23 +0100

    fin seance getOnRow

--- startup/app/controllers/productsController.php
+++ startup/app/controllers/productsController.php
@@ -10,11 +10,18 @@ class productsController extends ControllerBase{
 
 		$products=Products::find();
 		$lv=$semantic->dataTable("lv1-1","Products",$products);
-		$lv->setFields(["name","price"]);
-		$lv->setCaptions(["Nom : ","Prix :"]);
+		$lv->setFields(["name","price","active"]);
+		$lv->setCaptions(["Nom : ","Prix :","Actif :"]);
+		$lv->getOnRow("click","sTest/show","#lv2-6-detail",["attr"=>"data-ajax","preventDefault"=>false,"stopPropagation"=>false]);
 		//echo $lv;
 	
 		$this->jquery->compile($this->view);
 	}
+
+	public function showAction($id=1){
+        $produit = Products::findFirst($id);
+        echo "Nom : ".$produit->getName()."<br>";
+        echo "Prix : ".$produit->getPrice()."$";
+    }
 
 }
--- startup/app/views/products/index.volt
+++ startup/app/views/products/index.volt
@@ -1,4 +1,5 @@
 
 
 {{q["lv1-1"]}}
+<div id='lv2-6-detail'></div>
 {{script_foot}}
